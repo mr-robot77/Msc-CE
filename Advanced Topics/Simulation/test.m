a91ddef3b91d41f14b8659beebfaf263edcbbe0e
Saleh Abednezhad <abednezhad756@gmail.com>
2023-05-28 16:37:50 +0330

    Add files via upload

---
+++ Advanced Topics/Simulation/test.m
@@ -0,0 +1,11 @@
+function mnodes=test(n,x,y)
+rx = unifrnd(1:n, x); 
+ry = unifrnd(1:n, y);
+for j=1 : n
+    mnodes(j)= complex(rx(j), ry(j));
+end
+bsx=x/2;
+bsy=y/2;
+%plot(bsx,bsy,'ro') ;
+%hold;
+%plot(rx, ry, '*');
